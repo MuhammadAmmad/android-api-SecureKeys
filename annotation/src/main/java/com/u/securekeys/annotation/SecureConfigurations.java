16919c97c14ed67bf69046b6fb86bd4478db08a8
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-20 23:24:32 -0300

    Refine configurations class

--- annotation/src/main/java/com/u/securekeys/annotation/SecureConfigurations.java
+++ annotation/src/main/java/com/u/securekeys/annotation/SecureConfigurations.java
@@ -20,19 +20,29 @@ public @interface SecureConfigurations {
      * and will use default.
      * @return Initial vector used for aes with 16 bytes length
      */
-    byte[] aesInitialVector() default { 0x01, 0x02 };
+    byte[] aesInitialVector() default { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                                        0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
+                                        0x0e, 0x0f };
 
     /**
-     * 16 bytes length array for the aes key. If this is specified and has different size, exception will be thrown
+     * 32 bytes length array for the aes key. If this is specified and has different size, exception will be thrown
      * and will use default.
      *
-     * @return aes key used for aes with 16 bytes length
+     * @return aes key used for aes with 32 bytes length
      */
-    byte[] aesKey() default { 0x01, 0x02 };
+    byte[] aesKey() default   { 0x60, 0x3d, (byte) 0xeb, 0x10, 0x15, (byte) 0xca, 0x71,
+                                (byte) 0xbe, 0x2b, 0x73, (byte) 0xae, (byte) 0xf0, (byte) 0x85, 0x7d,
+                                0x77, (byte) 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b,
+                                0x61, 0x08, (byte) 0xd7, 0x2d, (byte) 0x98, 0x10, (byte) 0xa3,
+                                0x09, 0x14, (byte) 0xdf, (byte) 0xf4 };
 
     /**
      * Generate a random aes key and initial vector depending of the build. This will make on every build unique
      * hyperparameters for the aes cipher
+     *
+     * <b>Note:</b> If this is enabled, aesKey and aesInitialVector will be discarded (even if they are modified to something
+     * of your own)
+     *
      * @return boolean if should use a random aes key/iv or not.
      */
     boolean useAesRandomly() default false;
@@ -58,7 +68,7 @@ public @interface SecureConfigurations {
      *
      * @return list of allowed places from where the APK should be installed
      */
-    String[] permittedInstalledPlaces() default {};
+    String[] allowedInstallers() default {};
 
     /**
      * Signing certificate the APK should match, if it doesnt it will return empty strings
